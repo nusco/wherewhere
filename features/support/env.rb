5d4b1cfe57dc5cee6c9525358e64cd767ad58fc0
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2012-07-03 00:43:24 +0200

    silence Webkit JS-related warnings

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,10 @@ SITE = ENV['SITE'] || "localhost:9292"
 
 require 'capybara/cucumber'
 require 'capybara-webkit'
+# this silences the Webkit JS warnings by redirecting them to nil:
+Capybara.register_driver :webkit do |app|
+  Capybara::Driver::Webkit.new(app, :stdout => nil)
+end
 Capybara.default_driver = :webkit
 
 require './lib/wherewhere'
